72ea5e6dbebe7f7a41bbb13c96b1fa86b4b56c6e
Samuel_Lehnen <samuel.lehnen@katek-group.com>
2023-04-14 11:46:54 +0200

    Programmablaufplan erstellt

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,12 @@
 
 using Pin = uint8_t;
 
-enum class DisplayMode : uint8_t
-{
-    Dot,
-    Bar
-};
-
 constexpr uint8_t numLeds = 10;
-constexpr Pin ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+constexpr Pin ledPins[numLeds] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 constexpr Pin lowRefPin = A0;
 constexpr Pin highRefPin = A1;
 constexpr Pin signalPin = A2;
 constexpr Pin modePin = 12;
-
 
 
 uint16_t measureReference(Pin lowRefPin, Pin highRefPin)
@@ -41,69 +34,59 @@ void initializeDisplay(const Pin* ledPins, uint8_t numLeds)
     }
 }
 
-void displaySignal(uint16_t signal, uint16_t reference, DisplayMode mode, const Pin* ledPins, uint8_t numLeds)
+
+bool readMode(Pin modePin)
 {
-    float percentage = signal * 100.0 / reference;
+    return digitalRead(modePin);
+}
 
-    Serial.print("percentage: ");
-    Serial.print(percentage);
-    Serial.print("   ");
 
-    int16_t ledIndex = lrint(percentage * 0.1) - 1;
+int16_t calculateLedIndex(uint16_t signal, uint16_t reference, uint8_t numLeds)
+{
+    return lrint(static_cast<float>(signal) * numLeds / reference ) - 1;
+}
 
-    Serial.print("ledIndex: ");
-    Serial.print(ledIndex);
-    Serial.print("   ");
 
+void displaySignal(int16_t ledIndex, bool barMode, const Pin* ledPins, uint8_t numLeds)
+{
     for(uint8_t i = 0; i < numLeds; i++)
     {
         pinMode(ledPins[i], INPUT);
     }
 
-    switch(mode)
-    {
-        case DisplayMode::Bar:
-            for(uint8_t i = 0; i < ledIndex + 1; i++)
-            {
-                pinMode(ledPins[i], OUTPUT);
-                digitalWrite(ledPins[i], LOW);
-            }
-            break;
-        case DisplayMode::Dot:
-            pinMode(ledPins[ledIndex], OUTPUT);
-            digitalWrite(ledPins[ledIndex], LOW);
-            break;
+    if(ledIndex < 0)
+        return;
 
+    if(barMode)
+    {
+        for(uint8_t i = 0; i < ledIndex + 1; i++)
+        {
+            pinMode(ledPins[i], OUTPUT);
+            digitalWrite(ledPins[i], LOW);
+        }
+    }
+    else
+    {
+        pinMode(ledPins[ledIndex], OUTPUT);
+        digitalWrite(ledPins[ledIndex], LOW);
     }
 }
 
 
 void setup()
-{
-    Serial.begin(115200);
-    initializeDisplay(ledPins, numLeds);
-}
+{}
+
 
 void loop()
 {
-    uint16_t signal = measureSignal(signalPin);
-    uint16_t reference = measureReference(lowRefPin, highRefPin);
-
-    Serial.print("signal: ");
-    Serial.print(signal);
-    Serial.print("   ");
-
-    Serial.print("reference: ");
-    Serial.print(reference);
-    Serial.print("   ");
-
-    DisplayMode mode = static_cast<DisplayMode>(digitalRead(modePin));
-
-    Serial.print("mode: ");
-    Serial.print(static_cast<int>(mode));
-    Serial.print("   ");
-
-    displaySignal(signal, reference, mode, ledPins, numLeds);
-
-    Serial.println();
+    displaySignal(
+        calculateLedIndex(
+            measureSignal(signalPin), 
+            measureReference(lowRefPin, highRefPin),
+            numLeds
+        ),
+        readMode(modePin),
+        ledPins,
+        numLeds
+    );
 }
